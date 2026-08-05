bde069857ed092b980e801154bcab8597997620b
depot-data-thix-central <datathix@gmail.com>
2026-08-05 10:57:21 +0300

    Update thix_weeding_routes.dart

--- lib/presentation/thix_weeding/thix_weeding_routes.dart
+++ lib/presentation/thix_weeding/thix_weeding_routes.dart
@@ -31,12 +31,11 @@ import 'pages/staff/paiements/payments_page.dart';
 import 'pages/staff/paiements/add_payment_page.dart';
 import 'pages/staff/paiements/payment_detail_page.dart';
 import 'pages/staff/checklist/checklist_page.dart';
-import 'pages/staff/checklist/add_task_page.dart';
-import 'pages/staff/galerie/gallery_page.dart';
+import 'pages/staff/planning/task_form_page.dart';
+import 'pages/staff/galerie/gallery_management_page.dart';
 import 'pages/staff/livre_or/guestbook_management_page.dart';
 import 'pages/staff/invitation/preview_invitation_page.dart';
 import 'pages/staff/parametres/settings_page.dart';
-import 'pages/staff/rsvp/rsvp_management_page.dart';
 
 class ThixWeedingRoutes {
   static const String home = '/thix-weeding';
@@ -78,7 +77,8 @@ class ThixWeedingRoutes {
           pageBuilder: (context, state) => CustomTransitionPage(
             key: state.pageKey,
             child: const ThixWeedingHomePage(),
-            transitionsBuilder: (context, animation, secondaryAnimation, child) => FadeTransition(opacity: animation, child: child),
+            transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+                FadeTransition(opacity: animation, child: child),
           ),
           routes: [
             // ================= GUEST =================
@@ -95,18 +95,64 @@ class ThixWeedingRoutes {
                 return CustomTransitionPage(
                   key: state.pageKey,
                   child: GuestHomePage(weddingId: weddingId),
-                  transitionsBuilder: (context, animation, secondaryAnimation, child) => SlideTransition(position: Tween(begin: const Offset(1, 0), end: Offset.zero).animate(animation), child: child),
+                  transitionsBuilder:
+                      (context, animation, secondaryAnimation, child) =>
+                          SlideTransition(
+                    position: Tween(begin: const Offset(1, 0), end: Offset.zero)
+                        .animate(animation),
+                    child: child,
+                  ),
                 );
               },
               routes: [
-                GoRoute(path: 'invitation', name: invitation, builder: (context, state) => InvitationPage(weddingId: state.pathParameters['weddingId']!)),
-                GoRoute(path: 'programme', name: programme, builder: (context, state) => ProgrammePage(weddingId: state.pathParameters['weddingId']!)),
-                GoRoute(path: 'lieu', name: lieu, builder: (context, state) => LieuAccesPage(weddingId: state.pathParameters['weddingId']!)),
-                GoRoute(path: 'rsvp', name: rsvp, builder: (context, state) => RsvpPage(weddingId: state.pathParameters['weddingId']!)),
-                GoRoute(path: 'cadeaux', name: cadeaux, builder: (context, state) => CadeauxPage(weddingId: state.pathParameters['weddingId']!)),
-                GoRoute(path: 'galerie', name: galerie, builder: (context, state) => GaleriePage(weddingId: state.pathParameters['weddingId']!)),
-                GoRoute(path: 'livre-or', name: livreOr, builder: (context, state) => LivreOrPage(weddingId: state.pathParameters['weddingId']!)),
-                GoRoute(path: 'infos', name: infos, builder: (context, state) => InfosPage(weddingId: state.pathParameters['weddingId']!)),
+                GoRoute(
+                  path: 'invitation',
+                  name: invitation,
+                  builder: (c, s) =>
+                      InvitationPage(weddingId: s.pathParameters['weddingId']!),
+                ),
+                GoRoute(
+                  path: 'programme',
+                  name: programme,
+                  builder: (c, s) =>
+                      ProgrammePage(weddingId: s.pathParameters['weddingId']!),
+                ),
+                GoRoute(
+                  path: 'lieu',
+                  name: lieu,
+                  builder: (c, s) =>
+                      LieuAccesPage(weddingId: s.pathParameters['weddingId']!),
+                ),
+                GoRoute(
+                  path: 'rsvp',
+                  name: rsvp,
+                  builder: (c, s) =>
+                      RsvpPage(weddingId: s.pathParameters['weddingId']!),
+                ),
+                GoRoute(
+                  path: 'cadeaux',
+                  name: cadeaux,
+                  builder: (c, s) =>
+                      CadeauxPage(weddingId: s.pathParameters['weddingId']!),
+                ),
+                GoRoute(
+                  path: 'galerie',
+                  name: galerie,
+                  builder: (c, s) =>
+                      GaleriePage(weddingId: s.pathParameters['weddingId']!),
+                ),
+                GoRoute(
+                  path: 'livre-or',
+                  name: livreOr,
+                  builder: (c, s) =>
+                      LivreOrPage(weddingId: s.pathParameters['weddingId']!),
+                ),
+                GoRoute(
+                  path: 'infos',
+                  name: infos,
+                  builder: (c, s) =>
+                      InfosPage(weddingId: s.pathParameters['weddingId']!),
+                ),
               ],
             ),
 
@@ -114,43 +160,150 @@ class ThixWeedingRoutes {
             GoRoute(
               path: 'staff/my-weddings',
               name: staffMyWeddings,
-              builder: (context, state) => const MyWeddingsPage(),
+              builder: (c, s) => const MyWeddingsPage(),
             ),
             GoRoute(
               path: 'staff/:weddingId',
               name: staffDashboard,
-              builder: (context, state) => StaffShellPage(weddingId: state.pathParameters['weddingId']!),
+              builder: (c, s) => StaffShellPage(
+                weddingId: s.pathParameters['weddingId']!,
+              ),
               routes: [
-                // Invités
-                GoRoute(path: 'invites', name: staffInvites, builder: (c, s) => GuestListPage(weddingId: s.pathParameters['weddingId']!)),
-                GoRoute(path: 'invites/add', name: staffAddInvite, builder: (c, s) => AddGuestPage(weddingId: s.pathParameters['weddingId']!)),
-                GoRoute(path: 'invites/:guestId/edit', builder: (c, s) => AddGuestPage(weddingId: s.pathParameters['weddingId']!, editGuestId: s.pathParameters['guestId'])),
-
-                // Prestataires
-                GoRoute(path: 'prestataires', name: staffPrestataires, builder: (c, s) => VendorsListPage(weddingId: s.pathParameters['weddingId']!)),
-                GoRoute(path: 'prestataires/add', name: staffAddPrestataire, builder: (c, s) => AddVendorPage(weddingId: s.pathParameters['weddingId']!)),
-                GoRoute(path: 'prestataires/:vendorId/edit', builder: (c, s) => AddVendorPage(weddingId: s.pathParameters['weddingId']!, editVendorId: s.pathParameters['vendorId'])),
-
-                // Messages & Chat
-                GoRoute(path: 'messages', name: staffMessages, builder: (c, s) => StaffMessagesPage(weddingId: s.pathParameters['weddingId']!)),
-                GoRoute(path: 'messages/:guestId', name: staffChatDetail, builder: (c, s) => ChatDetailPage(weddingId: s.pathParameters['weddingId']!, guestId: s.pathParameters['guestId']!)),
-
-                // Budget & Paiements
-                GoRoute(path: 'budget', name: staffBudget, builder: (c, s) => BudgetPage(weddingId: s.pathParameters['weddingId']!)),
-                GoRoute(path: 'paiements', name: staffPaiements, builder: (c, s) => PaymentsPage(weddingId: s.pathParameters['weddingId']!)),
-                GoRoute(path: 'paiements/add', name: staffAddPaiement, builder: (c, s) => AddPaymentPage(weddingId: s.pathParameters['weddingId']!)),
-                GoRoute(path: 'paiements/:paymentId', name: staffPaiementDetail, builder: (c, s) => PaymentDetailPage(weddingId: s.pathParameters['weddingId']!, paymentId: s.pathParameters['paymentId']!)),
-
-                // Checklist
-                GoRoute(path: 'checklist', name: staffChecklist, builder: (c, s) => ChecklistPage(weddingId: s.pathParameters['weddingId']!)),
-                GoRoute(path: 'checklist/add', name: staffAddTask, builder: (c, s) => AddTaskPage(weddingId: s.pathParameters['weddingId']!)),
-
-                // Autres
-                GoRoute(path: 'galerie', name: staffGalerie, builder: (c, s) => GalleryPage(weddingId: s.pathParameters['weddingId']!)),
-                GoRoute(path: 'livre-or', name: staffLivreOr, builder: (c, s) => GuestbookManagementPage(weddingId: s.pathParameters['weddingId']!)),
-                GoRoute(path: 'invitation', name: staffInvitation, builder: (c, s) => PreviewInvitationPage(weddingId: s.pathParameters['weddingId']!)),
-                GoRoute(path: 'rsvp', name: staffRsvp, builder: (c, s) => RsvpManagementPage(weddingId: s.pathParameters['weddingId']!)),
-                GoRoute(path: 'parametres', name: staffSettings, builder: (c, s) => SettingsPage(weddingId: s.pathParameters['weddingId']!)),
+                GoRoute(
+                  path: 'invites',
+                  name: staffInvites,
+                  builder: (c, s) => GuestListPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'invites/add',
+                  name: staffAddInvite,
+                  builder: (c, s) => AddGuestPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'invites/:guestId/edit',
+                  builder: (c, s) => AddGuestPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                    editGuestId: s.pathParameters['guestId'],
+                  ),
+                ),
+                GoRoute(
+                  path: 'prestataires',
+                  name: staffPrestataires,
+                  builder: (c, s) => VendorsListPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'prestataires/add',
+                  name: staffAddPrestataire,
+                  builder: (c, s) => AddVendorPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'prestataires/:vendorId/edit',
+                  builder: (c, s) => AddVendorPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                    editVendorId: s.pathParameters['vendorId'],
+                  ),
+                ),
+                GoRoute(
+                  path: 'messages',
+                  name: staffMessages,
+                  builder: (c, s) => StaffMessagesPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'messages/:guestId',
+                  name: staffChatDetail,
+                  builder: (c, s) => ChatDetailPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                    guestIdOrName: s.pathParameters['guestId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'budget',
+                  name: staffBudget,
+                  builder: (c, s) => BudgetPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'paiements',
+                  name: staffPaiements,
+                  builder: (c, s) => PaymentsPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'paiements/add',
+                  name: staffAddPaiement,
+                  builder: (c, s) => AddPaymentPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'paiements/:paymentId',
+                  name: staffPaiementDetail,
+                  builder: (c, s) => PaymentDetailPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                    paymentId: s.pathParameters['paymentId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'checklist',
+                  name: staffChecklist,
+                  builder: (c, s) => ChecklistPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'checklist/add',
+                  name: staffAddTask,
+                  builder: (c, s) => TaskFormPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'galerie',
+                  name: staffGalerie,
+                  builder: (c, s) => GalleryManagementPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'livre-or',
+                  name: staffLivreOr,
+                  builder: (c, s) => GuestbookManagementPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'invitation',
+                  name: staffInvitation,
+                  builder: (c, s) => PreviewInvitationPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'rsvp',
+                  name: staffRsvp,
+                  builder: (c, s) => GuestListPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
+                GoRoute(
+                  path: 'parametres',
+                  name: staffSettings,
+                  builder: (c, s) => SettingsPage(
+                    weddingId: s.pathParameters['weddingId']!,
+                  ),
+                ),
               ],
             ),
           ],
